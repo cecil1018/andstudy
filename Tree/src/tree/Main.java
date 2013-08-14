f8003e965a7559d271f25260df5465988393e7f0
Hyeonseok cecil choi <hyeonseok.cecil.choi@gmail.com>
2013-08-14 11:17:16 +0900

    add test function

--- Tree/src/tree/Main.java
+++ Tree/src/tree/Main.java
@@ -2,18 +2,48 @@ package tree;
 
 public class Main {
 	public static void main(String [] args) {
+		integerTest();
+		stringTest();
+	}
+
+
+
+	private static <Type extends Comparable<Type>> void printResult(Tree<Type> tree) {
+		System.out.println("###### Print Tree");
+		tree.accept(new PrintTreeVisitor<Type>());
+		System.out.println("###### PreOrder");
+		tree.accept(new PreOrderVisitor<Type>());
+		System.out.println("###### InOrder");
+		tree.accept(new InOrderVisitor<Type>());
+		System.out.println("###### PostOrder");
+		tree.accept(new PostOrderVisitor<Type>());
+	}
+
+	private static void stringTest() {
+		Tree<String> tree = new Tree<String>();
+		
+		tree.add("Fxx");
+		tree.add("Cxx");
+		tree.add("Bxx");
+		tree.add("Dxx");
+		tree.add("Hxx");
+		tree.add("Gxx");
+		tree.add("Ixx");
+		
+		printResult(tree);
+	}
+	
+	private static void integerTest() {
 		Tree<Integer> tree = new Tree<Integer>();
 		
 		tree.add(5);
 		tree.add(3);
-		tree.add(6);
-		tree.add(2);
 		tree.add(1);
-		tree.add(0);
+		tree.add(4);
+		tree.add(7);
+		tree.add(6);
+		tree.add(8);
 		
-		System.out.println("###### PreOrder");
-		tree.accept(new PreOrderVisitor<Integer>());
-		System.out.println("###### InOrder");
-		tree.accept(new InOrderVisitor<Integer>());
+		printResult(tree);
 	}
 }
---
+++ Tree/src/tree/PrintTreeVisitor.java
@@ -0,0 +1,33 @@
+package tree;
+
+public class PrintTreeVisitor<Type extends Comparable<Type>> implements
+		Visitor<Type> {
+	private int depth;
+	
+	public PrintTreeVisitor() {
+		depth = 0;
+	}
+	
+	@Override
+	public void visit(BinaryNode<Type> dest) {
+		
+		for(int i = 0; i < depth; i++) {
+			System.out.print("\t");
+		}		
+		System.out.println(dest.getValue());
+		
+		if(dest.hasLeftChild()) {
+			depth ++;
+			dest.getLeftChild().accept(this);
+			depth --;
+		}
+		
+		if(dest.hasRightChild()) {
+			depth ++;
+			dest.getRightChild().accept(this);
+			depth --;
+		}
+		
+	}
+
+}
